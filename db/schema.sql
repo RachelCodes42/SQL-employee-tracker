b20fabf018cc50c09de2df1232a72eb19ea2eba3
Rachel Hochman <136940837+RachelCodes42@users.noreply.github.com>
2023-09-19 20:05:45 -0400

    package.json update and sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,33 @@
-/*----- Drop, Create and Use SQL DB -----*/
-DROP DATABASE IF EXISTS business_db;
-CREATE DATABASE business_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE business_db;
+USE employees_db;
 
-/*----- Create Tables  -----*/
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
+  title VARCHAR(255) NOT NULL,
   salary DECIMAL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
+  first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
---
+++ server.js
@@ -0,0 +1,174 @@
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+const express = require("express");
+// Import and require mysql2
+const mysql = require("mysql2");
+const { response } = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+
+// TODO: Create an array of prompt for user input
+function init() {
+  console.table("");
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "title",
+        choices: [
+          { name: "View all employees?", value: "View employees?" },
+          { name: "Add employee?", value: "Add employee?" },
+          { name: "Update employee role?", value: "Update employee role?" },
+          { name: "View all roles?", value: "View all roles?" },
+          { name: "Add role?", value: "Add role?" },
+          { name: "View all department?", value: "View all department?" },
+          { name: "Add department?", value: "Add department?" },
+          { name: "Remove department?", value: "Remove department?" },
+          { name: "View total budget by department?",value: "View total budget by department?",},
+          { name: "quit?", value: "quit?" },
+        ],
+      },
+    ])
+    .then((response) => {
+      let choice = response.title;
+      switch (choice) {
+        case "View employees?": {
+          viewEmployee();
+          break;
+        }
+
+        case "Add employee?": {
+          addEmployee();
+          break;
+        }
+
+        case "Update employee role?": {
+          updateEmployeeRole();
+          break;
+        }
+
+        case "View all roles?": {
+          viewAllRoles();
+          break;
+        }
+
+        case "Add role?": {
+          addRole();
+          break;
+        }
+
+        case "View all department?": {
+          viewAllDepartment();
+          break;
+        }
+
+        case "Add department?": {
+          addDepartment();
+          break;
+        }
+
+        case "Remove department":{
+          removeDepartment();
+          break;
+        }
+
+        case "View total budget by department?":{
+          viewTotalBudgetByDepartment()
+          break;
+        }
+
+        case "quit?": {
+          console.log("quit?");
+          break;
+        }
+      }
+      init();
+    });
+}
+
+function viewEmployee() {
+  console.log("I'm happy to help!");
+  db.query("SELECT * FROM employees", function (err, results) {
+    if (err) throw err;
+    console.table(results);
+  });
+  }
+
+  function viewAllRoles() {
+    console.log("I'm happy to help!");
+    db.query("SELECT * FROM role", function (err, results) {
+      if (err) throw err;
+      console.table(results);
+    });
+    }
+
+    function viewAllDepartment() {
+      console.log("I'm happy to help!");
+      db.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        console.table(results);
+      });
+      }
+
+
+  function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Enter the employee's first name:",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "Enter the employee's last name:",
+          name: "lastName",
+        },
+        {
+          type: "input",
+          message: "Enter the employee's role ID:",
+          name: "roleId",
+        },
+        {
+          type: "input",
+          message: "Enter the employee's manager ID:",
+          name: "managerId",
+        },
+      ])
+      .then((employee) => {
+        // Add the employee to the database here
+        console.log(`Employee ${employee.firstName} ${employee.lastName} added successfully.`);
+        init();
+      });
+
+      function addEmployee() {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Enter the employee's first name:",
+              name: "firstName",
+            },
+            {
+              type: "input",
+              message: "Enter the employee's last name:",
+              name: "lastName",
+            },
+          ])
+          .then((answers) => {
+            console.log(`Employee's first name: ${answers.firstName}`);
+            console.log(`Employee's last name: ${answers.lastName}`);
+          });
+      }
+  }
+  
+// Function call to initialize app
+init();
